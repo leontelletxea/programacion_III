60a872bac2586af73b5dcb7ab336dae73e57c317
Leon Telletxea <leontelletxea@gmail.com>
2021-04-09 11:13:43 -0300

    Commit

--- clase01/guia/ejercicio01.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $array = [1=>["Perro", "Gato", "Ratón", "Araña", "Mosca"], 2=>["1986", "1996", "2015", "78", "86"], 3=>["php", "mysql", "html5", "typescript", "ajax"]];
-
-    echo '<pre>' , var_dump($array) , '</pre>';
-
-?>
